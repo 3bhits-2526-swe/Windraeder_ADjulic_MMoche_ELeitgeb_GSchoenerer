e230fe9fc65b1353c5aefb1ffd9bed4b626ac7f2
GSchoenerer <georg.schoenerer09@htl-salzburg.ac.at>
2026-05-29 13:14:01 +0200

    basic test structure

---
+++ WindratSimulation/Assets/myGame/Scripts/Test_APIPull.cs
@@ -0,0 +1,21 @@
+using System.Runtime.CompilerServices;
+using TMPro;
+using UnityEngine;
+
+
+
+public class Test_APIPull : MonoBehaviour
+{
+    [SerializeField] TMP_Text output;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
